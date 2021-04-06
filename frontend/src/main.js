f1b782a784dc6a600d2a5ded060e7f5c97ed3a8e
jianghaotian <jianghaotian126@126.com>
2021-04-06 23:14:42 +0800

    feat(frontend): 创建前端项目

---
+++ frontend/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+
+import 'normalize.css'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss'
+
+import App from './App.vue'
+import store from './store'
+import router from './router'
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App)
+}).$mount('#app')
