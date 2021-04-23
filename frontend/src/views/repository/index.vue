e8c83cd1a798155eeca952fdedaf07729db3a9c9
Fankexin <974094944@qq.com>
2021-04-23 17:47:00 +0800

    仓库右侧标签v_1.0

--- frontend/src/views/repository/index.vue
+++ frontend/src/views/repository/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="repository">
-    <h1>仓库</h1>
+    <div>1</div>
+    <div class="rightTab">
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="项目动态" name="MainFirst">
+          <div>项目动态</div>
+        </el-tab-pane>
+        <el-tab-pane label="项目成员" name="MainSecond">
+          <div>项目成员</div>
+        </el-tab-pane>
+        <el-tab-pane label="项目设置" name="MainThird">
+          <div>项目设置</div>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
   </div>
 </template>
 
@@ -9,11 +22,26 @@
 
 export default {
   name: 'Repository',
-  components: {}
+  components: {},
+  data() {
+    return {
+      activeName: 'MainFirst'
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 .repository {
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 100%;
+  .rightTab {
+    width: 290px;
+    padding: 0 20px;
+    border-left: 1px solid #e4e7ed;
+  }
 }
 </style>
