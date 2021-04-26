f38d8f22d3e0fe388921ded6f6fccc1ec83165d4
Fankexin <974094944@qq.com>
2021-04-26 12:01:25 +0800

    項目動態v_1.0

--- frontend/src/views/repository/index.vue
+++ frontend/src/views/repository/index.vue
@@ -3,7 +3,7 @@
     <el-container style="height: 100%">
       <el-main>
         {{ url }}
-        <el-row>
+        <el-row style="padding: 10px">
           <el-dropdown @command="createView">
             <el-button size="mini" icon="el-icon-circle-plus-outline"
               >新建<i class="el-icon-arrow-down el-icon--right"></i
@@ -16,6 +16,7 @@
           <el-button size="mini" icon="el-icon-upload" style="margin-left: 10px">上传</el-button>
           <el-button size="mini" icon="el-icon-delete">删除</el-button>
         </el-row>
+        <el-row style="padding: 10px">全部文件({{ content.length }})</el-row>
         <div>
           <el-table
             ref="table"
@@ -44,15 +45,27 @@
         </div>
       </el-main>
       <el-aside class="rightTab" width="290px">
-        <el-tabs v-model="activeName">
-          <el-tab-pane label="项目动态" name="MainFirst">
-            <div>项目动态</div>
+        <el-tabs v-model="activeName" style="height: 100%">
+          <el-tab-pane label="项目动态" name="MainFirst" style="height: 100%">
+            <div class="dynamics">
+              <div v-for="item in operationsHistory" :key="item.id" class="box-card">
+                <!-- <el-card class="box-card">
+                  {{ '列表内容 ' + item }}
+                </el-card> -->
+                <el-avatar :size="32" src="https://empty">
+                  <img src="@/assets/images/default-user.png" />
+                </el-avatar>
+                <span class="name">{{ item.name }}</span>
+                <span class="operations">{{ item.operations }}</span>
+                <span class="time">{{ item.time }}</span>
+              </div>
+            </div>
           </el-tab-pane>
           <el-tab-pane label="项目成员" name="MainSecond">
-            <div>项目成员</div>
+            <div class="members">项目成员</div>
           </el-tab-pane>
           <el-tab-pane label="项目设置" name="MainThird">
-            <div>项目设置</div>
+            <div class="setup">项目设置</div>
           </el-tab-pane>
         </el-tabs>
       </el-aside>
@@ -71,13 +84,15 @@ export default {
       activeName: 'MainFirst',
       url: this.$route.params.id,
       isSearchLoading: false,
-      content: []
+      content: [],
+      operationsHistory: []
     }
   },
   watch: {
     $route: function () {
       this.url = this.$route.params.id
       // console.log(from, to)
+      this.initData()
     }
   },
   created() {
@@ -86,22 +101,44 @@ export default {
   methods: {
     initData() {
       this.isSearchLoading = true
-      let contentData = [
-        {
-          id: 1,
-          name: '1.xlsx',
-          update_time: '2021-04-25 20:01:02',
-          update_person: 'nancy',
-          size: '25B'
-        },
-        { id: 2, name: '2.txt', update_time: '2021-04-25 20:02:02', update_person: 'nancy', size: '30B' },
-        { id: 3, name: '3.docx', update_time: '2021-04-25 20:03:02', update_person: 'nancy', size: '50B' },
-        { id: 4, name: '4', update_time: '2021-04-25 20:04:02', update_person: 'nancy' }
-      ]
-      for (let i = 0; i < contentData.length; i++) {
-        let type = contentData[i].name.split('.')[contentData[i].name.split('.').length - 1].toLocaleLowerCase()
+      if (this.url == 3) {
+        this.content = [
+          {
+            name: '1.xlsx',
+            update_time: '2021-04-25 20:01:02',
+            update_person: 'nancy',
+            size: '25B'
+          },
+          { name: '2.txt', update_time: '2021-04-25 20:02:02', update_person: 'nancy', size: '30B' },
+          { name: '3', update_time: '2021-03-25 20:04:02', update_person: 'nancy' }
+        ]
+        this.operationsHistory = [
+          { id: '1', operations: '添加了1.xlsx', name: 'nancy', time: '2021-04-25 20:01:02' },
+          { id: '2', operations: '添加了2.txt', name: 'nancy', time: '2021-04-25 20:02:02' },
+          { id: '3', operations: '添加了3', name: 'nancy', time: '2021-03-25 20:04:02' }
+        ]
+      } else {
+        this.content = [
+          {
+            name: '1.mp4',
+            update_time: '2021-04-25 20:01:02',
+            update_person: 'nancy',
+            size: '25B'
+          },
+          { name: '2.mp3', update_time: '2021-04-25 20:02:02', update_person: 'nancy', size: '30B' },
+          { name: '3.zip', update_time: '2021-04-25 20:03:02', update_person: 'nancy', size: '50B' },
+          { name: '4.ppt', update_time: '2021-04-25 20:04:02', update_person: 'nancy' }
+        ]
+        this.operationsHistory = [
+          { id: '4', operations: '添加了4.ppt', name: 'nancy', time: '2021-04-25 20:04:02' },
+          { id: '2', operations: '添加了2.mp3', name: 'nancy', time: '2021-04-25 20:02:02' },
+          { id: '3', operations: '添加了3.zip', name: 'nancy', time: '2021-04-25 20:03:02' }
+        ]
+      }
+      for (let i = 0; i < this.content.length; i++) {
+        let type = this.content[i].name.split('.')[this.content[i].name.split('.').length - 1].toLocaleLowerCase()
         let icon = ''
-        if (contentData[i].name.split('.').length == 1) {
+        if (this.content[i].name.split('.').length == 1) {
           icon = '#icon-file-b-2'
         } else {
           switch (type) {
@@ -141,9 +178,8 @@ export default {
           }
         }
 
-        contentData[i].icon = icon
+        this.content[i].icon = icon
       }
-      this.content = contentData
       this.isSearchLoading = false
     },
     onSelect(val) {
@@ -163,8 +199,40 @@ export default {
 .repository {
   height: 100%;
   .rightTab {
-    padding: 0 20px;
     border-left: 1px solid #e4e7ed;
+    .dynamics {
+      width: 100%;
+      height: 100%;
+      padding: 10px 20px 0;
+      background-color: #e4e7ed;
+      overflow-y: auto;
+      .box-card {
+        width: 100%;
+        position: relative;
+        font-size: 12px;
+        margin-bottom: 10px;
+        .name {
+          position: absolute;
+          top: 0;
+          left: 40px;
+        }
+        .operations {
+          position: absolute;
+          top: 20px;
+          left: 40px;
+        }
+        .time {
+          position: absolute;
+          top: 0;
+          right: 0;
+        }
+      }
+    }
   }
+}
+</style>
+<style>
+.el-tabs__content {
+  height: calc(100% - 40px);
 }
 </style>
